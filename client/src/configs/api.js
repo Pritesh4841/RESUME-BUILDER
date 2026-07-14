db3f5407e53a13b663f4d84581a82db9c6ce3bbb
Pritesh Parmar <priteshparmar4841@gmail.com>
2026-07-14 15:37:54 +0530

    Update api.js

--- client/src/configs/api.js
+++ client/src/configs/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const api = axios.create({
-    baseURL: https://resume-builder-6s1y.onrender.com
+    baseURL: "https://resume-builder-6s1y.onrender.com"
 })
 
 export default api
